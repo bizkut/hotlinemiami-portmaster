f2c0d99eacd990abb0440f51b6463115682692b7
bizkut <bizkut@users.noreply.github.com>
2025-12-17 19:30:01 +0800

    v1.1.0: Switch to gmloadernext.aarch64 with bundled libs (Undertale pattern)

--- Hotline Miami.sh
+++ Hotline Miami.sh
@@ -13,31 +13,26 @@ else
 fi
 
 source $controlfolder/control.txt
-source $controlfolder/device_info.txt
-
-export PORT_32BIT="Y" 
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 get_controls
 
+# Variables
 GAMEDIR="/$directory/ports/hotlinemiami"
+GMLOADER_JSON="$GAMEDIR/gmloader.json"
+
+# CD and set permissions
 cd $GAMEDIR
-
-# Exports
-export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$LD_LIBRARY_PATH"
-export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
-export GMLOADER_DEPTH_DISABLE=1
-export GMLOADER_SAVEDIR="$GAMEDIR/gamedata/"
-export GMLOADER_PLATFORM="os_linux"
-
-# We log the execution of the script into log.txt
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-$ESUDO chmod 666 /dev/uinput
-$GPTOKEYB "gmloader" -c "hotlinemiami.gptk" &
-$ESUDO chmod +x "$GAMEDIR/gmloader"
-./gmloader game.port
+# Exports
+export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/lib:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+$ESUDO chmod +x $GAMEDIR/gmloadernext.aarch64
+
+# Assign configs and load the game
+$GPTOKEYB "gmloadernext.aarch64" -c "hotlinemiami.gptk" &
+pm_platform_helper "$GAMEDIR/gmloadernext.aarch64"
+./gmloadernext.aarch64 -c "$GMLOADER_JSON"
 
 # Cleanup
-$ESUDO kill -9 $(pidof gptokeyb)
-$ESUDO systemctl restart oga_events &
-printf "\033c" > /dev/tty0
+pm_finish
