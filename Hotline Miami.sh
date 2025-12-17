ebeb6406e87054425e43c8c6039206f795dcb4b3
bizkut <bizkut@users.noreply.github.com>
2025-12-17 19:20:23 +0800

    Major refactor: use original gmloader (32-bit) like Maldita Castilla

--- Hotline Miami.sh
+++ Hotline Miami.sh
@@ -13,56 +13,31 @@ else
 fi
 
 source $controlfolder/control.txt
+source $controlfolder/device_info.txt
+
+export PORT_32BIT="Y" 
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 get_controls
 
-# Variables
 GAMEDIR="/$directory/ports/hotlinemiami"
-
-# CD and set logging
 cd $GAMEDIR
-> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
-
-# Determine architecture and set gmloader binary
-if [ "$DEVICE_ARCH" == "aarch64" ]; then
-    GMLOADER="gmloadernext.aarch64"
-    LIBDIR="lib/arm64-v8a"
-    SYSLIB="/usr/lib"
-else
-    GMLOADER="gmloadernext.armhf"
-    LIBDIR="lib/armeabi-v7a"
-    SYSLIB="/usr/lib32"
-fi
-
-# Setup permissions
-$ESUDO chmod +xwr "$GAMEDIR/$GMLOADER"
-
-# Remove any bad symlinks and only create libm.so symlink
-$ESUDO rm -f "$GAMEDIR/$LIBDIR/libc.so" 2>/dev/null
-$ESUDO rm -f "$GAMEDIR/$LIBDIR/libdl.so" 2>/dev/null
-$ESUDO rm -f "$GAMEDIR/$LIBDIR/libpthread.so" 2>/dev/null
-$ESUDO rm -f "$GAMEDIR/$LIBDIR/liblog.so" 2>/dev/null
-
-# Only symlink libm.so if needed
-if [ ! -f "$GAMEDIR/$LIBDIR/libm.so" ]; then
-    $ESUDO ln -sf "$SYSLIB/libm.so" "$GAMEDIR/$LIBDIR/libm.so"
-fi
 
 # Exports
-export LD_LIBRARY_PATH="$GAMEDIR/$LIBDIR:$SYSLIB:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+export GMLOADER_DEPTH_DISABLE=1
+export GMLOADER_SAVEDIR="$GAMEDIR/gamedata/"
+export GMLOADER_PLATFORM="os_linux"
 
-# Display loading splash
-if [ -f "$GAMEDIR/splash.png" ]; then
-    $ESUDO chmod +xr "$GAMEDIR/tools/splash" 2>/dev/null
-    [ "$CFW_NAME" == "muOS" ] && $ESUDO "$GAMEDIR/tools/splash" "$GAMEDIR/splash.png" 1 2>/dev/null
-    $ESUDO "$GAMEDIR/tools/splash" "$GAMEDIR/splash.png" 2000 2>/dev/null &
-fi
+# We log the execution of the script into log.txt
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-# Assign gptokeyb and load the game
-$GPTOKEYB "$GMLOADER" -c "hotlinemiami.gptk" &
-pm_platform_helper "$GAMEDIR/$GMLOADER" >/dev/null
-./$GMLOADER -c gmloader.json
+$ESUDO chmod 666 /dev/uinput
+$GPTOKEYB "gmloader" -c "hotlinemiami.gptk" &
+$ESUDO chmod +x "$GAMEDIR/gmloader"
+./gmloader game.port
 
 # Cleanup
-pm_finish
+$ESUDO kill -9 $(pidof gptokeyb)
+$ESUDO systemctl restart oga_events &
+printf "\033c" > /dev/tty0
